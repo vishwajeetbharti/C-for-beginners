c2e00bc303bb9e070df657c4cb63d4b1d6e95b32
Vishwaajeet Bharti <71969138+vishwajeetbharti@users.noreply.github.com>
2021-03-14 15:36:00 +0530

    2D array

---
+++ array2D.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+int a[2][2];
+int i,j;
+for(i=0;i<=2;i++)
+{
+	for(j=0;j<=2;j++)
+	{
+		scanf("%d",&a[i][j]);
+	}
+}
+for(i=0;i<=2;i++)
+{
+	for(j=0;j<=2;j++)
+	{
+		printf("%d\t",a[i][j]);
+	}
+	printf("\n");
+}
+
+}
