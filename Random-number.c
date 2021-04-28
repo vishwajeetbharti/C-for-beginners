65caa4ac9d6b25108781cf7c9e24da9cd08647b0
Vishwaajeet Bharti <71969138+vishwajeetbharti@users.noreply.github.com>
2021-04-28 10:30:57 +0530

    To generate random number.

---
+++ Random-number.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void printRandoms(int lower, int upper,
+							int count)
+{
+	int i;
+	for (i = 0; i < count; i++) {
+		int num = (rand() %
+		(upper - lower + 1)) + lower;
+		printf("%d ", num);
+	}
+}
+int main()
+{
+	int lower,upper, count = 1;
+	printf("Enter the lower and upper number:-\n");
+	scanf("%d%d",&lower,&upper);
+	srand(time(0));
+
+	printRandoms(lower, upper, count);
+
+	return 0;
+}
+
